c22694d095673ca327b183df9500cdf016d2ac16
blerou <sulik.szabolcs@gmail.com>
2016-05-19 22:09:51 +0200

    update packages

---
+++ elpa/deferred-20160109.2046/deferred-autoloads.el
@@ -0,0 +1,15 @@
+;;; deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("deferred.el") (22236 37808 931300 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; deferred-autoloads.el ends here
