4e8700127b35ce204929cc84d07f498f545be65d
blerou <sulik.szabolcs@gmail.com>
2018-06-10 14:47:34 +0200

    update

---
+++ elpa/with-namespace-1.1/with-namespace-autoloads.el
@@ -0,0 +1,16 @@
+;;; with-namespace-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("with-namespace.el") (23250 26923 708528
+;;;;;;  175000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; with-namespace-autoloads.el ends here
---
+++ elpa/with-namespace-1.1/with-namespace-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "with-namespace" "1.1" "Poor-man's namespaces for elisp" 'nil)
