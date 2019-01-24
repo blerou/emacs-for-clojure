4fc44c960ae38f6c0281132834a287a29450eef6
blerou <sulik.szabolcs@gmail.com>
2019-01-24 13:18:11 +0100

    company speed improvement

--- init.el
+++ init.el
@@ -246,6 +246,11 @@
 
 (set-face-attribute 'default nil :height 150)
 
+;; ## company-mode
+
+(setq company-dabbrev-downcase 0)
+(setq company-idle-delay 0)
+
 ;; ### Languages
 
 ;; # Elisp
@@ -281,11 +286,11 @@
 ;; ## Cider
 
 ;; go right to the REPL buffer when it's finished connecting
-(setq cider-repl-pop-to-buffer-on-connect t)
+;(setq cider-repl-pop-to-buffer-on-connect t)
 
 ;; When there's a cider error, show its buffer and switch to it
 (setq cider-show-error-buffer t)
-(setq cider-auto-select-error-buffer t)
+;(setq cider-auto-select-error-buffer t)
 
 ;; Where to store the cider history.
 (setq cider-repl-history-file "~/.emacs.d/cider-history")
@@ -297,8 +302,8 @@
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
 ;; fuzzy completion
-(add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
-(add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-repl-mode-hook 'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook 'cider-company-enable-fuzzy-completion)
 
 ;; custom keys
 (add-hook 'cider-mode-hook
