483acb5dae823ee47c6198f10f52c2a76b2c6aab
blerou <sulik.szabolcs@gmail.com>
2019-06-18 11:17:20 +0200

    use melpa instead of melpa stable. add `ein`

--- init.el
+++ init.el
@@ -11,7 +11,8 @@
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 ;;                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://stable.melpa.org/packages/")))
+;;                         ("melpa" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
 
 ;; Load and activate emacs packages. Do this first so that the
@@ -235,7 +236,7 @@
  '(fci-rule-color "#00346e")
  '(package-selected-packages
    (quote
-    (ensime groovy-mode gradle-mode scala-mode company-terraform terraform-mode lua-mode eyebrowse flycheck-joker flycheck-clojure flycheck-rust racer cargo rust-playground rust-mode ag php-mode dumb-jump company which-key with-namespace yaml-mode tagedit smex showkey projectile paredit markdown-mode magit jedi ido-ubiquitous go-mode function-args find-file-in-repository exec-path-from-shell clojure-mode-extra-font-locking cider-eval-sexp-fu autopair ac-cider))))
+    (ein ensime groovy-mode gradle-mode scala-mode company-terraform terraform-mode lua-mode eyebrowse flycheck-joker flycheck-clojure flycheck-rust racer cargo rust-playground rust-mode ag php-mode dumb-jump company which-key with-namespace yaml-mode tagedit smex showkey projectile paredit markdown-mode magit jedi ido-ubiquitous go-mode function-args find-file-in-repository exec-path-from-shell clojure-mode-extra-font-locking cider-eval-sexp-fu autopair ac-cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
